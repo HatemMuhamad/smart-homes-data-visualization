e42d70ce981564ff234f9c9d1af90f8e32a366e0
Hatem Muhamad <hatemmuhamad@Hatems-MacBook-Pro.local>
2022-03-21 18:34:05 -0600

    Front end inital setup

---
+++ react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyApp from './components/MyApp/MyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <MyApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/common/Types.tsx
@@ -0,0 +1,17 @@
+// export interface Reading {
+//     Serial_Number: string;
+//     DateTime: Date;
+//     Device_ID: string;
+//     Device_Name: string;
+//     User_Device_Name: string;
+//     Device_Type: string;
+//     Device_Make: string;
+//     Device_Model: string;
+//     Device_Location: string;
+//     Wattage: number;
+// }
+
+export interface Reading {
+    day: string;
+    Wattage_Daily_Sum: number;
+}
