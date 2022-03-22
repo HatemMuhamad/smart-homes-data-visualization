b3afbafe2e85be1876b47b0703a0d3fa9d1b752c
Hatem Muhamad <hatemmuhamad@Hatems-MacBook-Pro.local>
2022-03-21 23:16:33 -0600

    readme updated

--- react-app/src/api/BaseApi.tsx
+++ react-app/src/api/BaseApi.tsx
@@ -4,7 +4,6 @@ let axiosInstance: AxiosInstance;
 const axiosBaseConfig = {
   baseURL: process.env.REACT_APP_SERVER_API_URL,
   timeout: 10000 * 20,
-  //withCredentials: true,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
